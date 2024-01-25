400cfa2fe9ffa2d162ad1b820f1fae385681f5b2
ikari2k <pluta.m@gmail.com>
2024-01-25 11:10:43 +0100

    add multiple lines

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], 
              align='L', ln=1)
-    pdf.line(10, 21, 200, 21)
+
+    #Set lines
+    for i in range(20,298,10):
+        pdf.line(10, i, 200, i)
 
     #Set Footer
     pdf.ln(265)
@@ -24,6 +27,11 @@ for index, row in df.iterrows():
 
     for i in range(row['Pages'] - 1):
         pdf.add_page()
+
+        #Set lines
+        for i in range(20,298,10):
+            pdf.line(10, i, 200, i)
+
         #Set Footer
         pdf.ln(277)
         pdf.set_font(family='Times', style="I",size=8)
